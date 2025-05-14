7a003940c5d398e9a5211ffb22fc06573851e3b1
Augustvilliam <willyp@hotmail.se>
2025-05-14 15:51:20 +0200

    Addeed Jwt

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,21 +1,55 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Presentation.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DefaultCors", policy =>
+    policy.AllowAnyHeader()
+    .AllowAnyHeader()
+    .AllowAnyOrigin());
+});
 builder.Services.AddDbContext<DataContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("EventConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EventConnection")));
 
 builder.Services.AddOpenApi();
+var jwtSection = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSection["Key"]);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = true;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSection["Issuer"],
+        ValidAudience = jwtSection["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
 
 var app = builder.Build();
-app.MapOpenApi();
+
 app.UseHttpsRedirection();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyHeader().AllowAnyMethod());
+app.UseCors("DefaultCors");
 
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
+app.MapOpenApi();
 app.Run();
